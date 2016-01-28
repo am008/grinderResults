4541d8d2ec56c16ba3f2621286424cbaf2ea5d50
ak008 <akshaymuchandi@gmail.com>
2016-01-28 20:29:26 +0530

    Sorted Date

--- src/chart/chRT2.java
+++ src/chart/chRT2.java
@@ -4,18 +4,26 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.io.File;
 import java.io.FileReader;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
+import org.jfree.data.general.Dataset;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.DateAxis;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.time.Day;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.time.TimeSeriesDataItem;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
@@ -58,7 +66,7 @@ public class chRT2 {
     int count=0;
     try{
       final JSONParser parser = new JSONParser();
-      final Object obj = parser.parse(new FileReader(filename));
+      final Object obj = parser.parse(new FileReader("data/"+filename+".json"));
       final JSONObject jsonObject = (JSONObject) obj;
       final JSONObject reftests = (JSONObject) jsonObject.get("css21-reftests");
       final JSONArray results = (JSONArray) reftests.get("results");
@@ -80,20 +88,22 @@ public class chRT2 {
   
 
   public static void main(final String[] args) throws Exception {
-    final JFreeChart xylineChart = ChartFactory.createXYLineChart(
+    /*final JFreeChart xylineChart = ChartFactory.createXYLineChart(
         "Web-Platform Test Results", "Tests Conducted", "Tests Failed",
         configParser("web-platform-files"), PlotOrientation.VERTICAL, true, true,
-        false);
+        false);*/
     
-    final JFreeChart grinderChart = ChartFactory.createXYLineChart(
+    /*final JFreeChart grinderChart = ChartFactory.createXYLineChart(
         "Grinder Test Results", "Tests Conducted", "Tests Failed",
         configParser("grinder-files"), PlotOrientation.VERTICAL, true, true,
-        false);
+        false);*/
+    
+    final JFreeChart grinderChart = ChartFactory.createTimeSeriesChart("Grinder Test Results","Date","Tests Failed",configParser("grinder-files"),true,true,false);
     
     System.out.println("\n*********Creating charts*********");
     
-    System.out.println("Creating web-platform test chart");
-    exportChart(xylineChart,"Chart1.PNG");
+    //System.out.println("Creating web-platform test chart");
+    //exportChart(xylineChart,"Chart1.PNG");
     System.out.println("\nCreating grinder test chart");
     exportChart(grinderChart,"Chart2.PNG");
   }
@@ -115,8 +125,12 @@ public class chRT2 {
     plot.setDomainGridlinePaint(Color.DARK_GRAY);
     plot.setRangeGridlinePaint(Color.DARK_GRAY);
     
-    final NumberAxis domainAxis = (NumberAxis) plot.getDomainAxis();
-    domainAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+ 
+    DateAxis axis = (DateAxis) plot.getDomainAxis();
+    axis.setDateFormatOverride(new SimpleDateFormat("dd-MM-yyyy"));
+    
+    //final NumberAxis domainAxis = (NumberAxis) plot.getDomainAxis();
+    //domainAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
    
     final File XYLine = new File(fileName);
     ChartUtilities.saveChartAsPNG(XYLine, chart , 500, 500);
@@ -124,31 +138,46 @@ public class chRT2 {
   }
   
 
-  public static XYDataset configParser(String testName){
+  public static TimeSeriesCollection configParser(String testName){
     List<Integer> ResultList;
     XYSeries newSeries;
+    TimeSeries newTimeSeries;
     final XYSeriesCollection dataSet = new XYSeriesCollection();
+    final TimeSeriesCollection timeSeriesdataset = new TimeSeriesCollection();
     
     try {
       System.out.println("\nProcessing "+ testName +" test Results");
       final JSONParser parser = new JSONParser();
-      final JSONArray readfile = (JSONArray) parser.parse(new FileReader("config.json"));
+      final JSONObject readfile = (JSONObject) parser.parse(new FileReader("Config.json"));
+      
+      final List<String> dates= (List<String>) readfile.get("Dates");
+      
+      final JSONArray files = (JSONArray) readfile.get("Files");
 
-      for (final Object o : readfile) {
+      for (final Object o : files) {
         final JSONObject browser = (JSONObject) o;
         final String browsername = (String) browser.get("name");
         System.out.println("Processing: " + browsername);
         
         final JSONArray TestArrayFiles = (JSONArray) browser.get(testName);
       
-        ResultList = jsonTestFileResult(TestArrayFiles,testName);
-        newSeries = createSeries(ResultList,browsername);
-        dataSet.addSeries(newSeries);
+        //if(testName=="web-platform-files")
+        //{
+          //ResultList = jsonTestFileResult(TestArrayFiles,testName);
+          //newSeries = createSeries(ResultList,browsername);
+          //dataSet.addSeries(newSeries);
+        //}
+        //else
+        //{
+          ResultList = jsonTestFileResult(TestArrayFiles,testName);
+          newTimeSeries = timeSeries(ResultList,browsername,dates);
+          timeSeriesdataset.addSeries(newTimeSeries);
+        //}
      }
     }catch(final Exception e){
       e.printStackTrace();
     }
-    return dataSet;
+    return timeSeriesdataset;
   }
     
   
@@ -167,7 +196,7 @@ public class chRT2 {
   
   
   public static XYSeries createSeries(List<Integer> ResultList,String browsername){
-    final XYSeries Series = new XYSeries(browsername);
+    final XYSeries Series = new XYSeries(browsername);  
     
     int i=1;
     for(int result : ResultList){
@@ -177,4 +206,20 @@ public class chRT2 {
     return Series;
   }
   
+  @SuppressWarnings("deprecation")
+  public static TimeSeries timeSeries(List<Integer> ResultList,String browsername,List<String> dates){
+    TimeSeries Series = new TimeSeries(browsername, Day.class);
+    int len = dates.size();
+    
+    for(int i=0;i<len;i++)
+    {
+      int year = Integer.parseInt(dates.get(i).split("-")[0]);
+      int  month =Integer.parseInt(dates.get(i).split("-")[1]);
+      int date =Integer.parseInt(dates.get(i).split("-")[2]);
+      
+      Series.addOrUpdate(new Day(date,month,year),ResultList.get(i));
+     
+    }
+    return Series;
+  }
 }
